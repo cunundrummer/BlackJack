e8a372a5132ade6f274c5e43406d8a21f314e9f0
cunundrummer <leeranGuest@Leerans-MacBook-Pro.local>
2016-03-18 14:42:38 -0400

    Moved text functions in main to testFunctions files
    same as title

---
+++ BlackJack/testFunctions.hpp
@@ -0,0 +1,35 @@
+//
+//  testFunctions.hpp
+//  BlackJack
+//
+//  Created by Leeran on 2016-03-18.
+//  Copyright © 2016 Compulsive Gamblers inc. All rights reserved.
+//
+
+#ifndef testFunctions_hpp
+#define testFunctions_hpp
+
+#include <iostream>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include "input.h"
+#include "prompts.h"
+#include "game.h"
+#include "player.h"
+#include "card.h"
+#include "cardpile.h"
+#include "hand.h"
+#include "deck.h"
+#include "gamePlayer.h"
+#include "menu.h"
+#include "player.h"
+
+void testMenu();
+void testPlayer();
+void testAskForInsurance();
+void testInsurance();
+
+
+#endif /* testFunctions_hpp */
